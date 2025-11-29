2fda567cc016d82c354904eda7dfaf312cecea65
igris-arise95 <demonclass.com@gmail.com>
2025-11-29 22:31:31 +0530

    commit index.html

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User, Blog
+from forms import RegistrationForm, LoginForm, BlogForm
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
